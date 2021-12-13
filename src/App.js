9aa7cfd3d30a7b4f72cdecdf05d4a10ba84bb277
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-12-13 13:45:08 +0530

    AXIOS_DATA_SAVED

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import CollectionCard from "./components/CollectionCard";
-
-
-
+import axios from "axios";
 
 function App() {
+  const [punkListData, setPunkListData] = useState([]);
 
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get(
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0xc6616470527ccE3B507DE0bc742a459d98ed2fAD&order_direction=asc"
+      );
+      // console.log(openseaData.data.assets);
+      setPunkListData(openseaData.data.assets);
+    };
+    return getMyNfts();
+  }, []);
 
   return (
     <div className="app">
