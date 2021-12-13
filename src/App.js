5bcfee8aee272d4b52847120c1cbd2add47260a0
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-12-13 12:17:54 +0530

    CollectionCard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from "./logo.svg";
+import {useState, useEffect} from 'react';
 import "./App.css";
 import Header from "./components/Header";
+import CollectionCard from "./components/CollectionCard";
+
+
+
 
 function App() {
+
+
   return (
     <div className="app">
       <Header />
+      <CollectionCard
+        id={0}
+        name={"Bandana Punk"}
+        traits={[
+          {
+            value: 7,
+          },
+        ]}
+        image="https://nftlabs.mypinata.cloud/ipfs/bafybeigqkficum3anns36jid3dxvc4yfauyuvqjulbg43n23qxn3ce3tyu"
+      />
     </div>
   );
 }
